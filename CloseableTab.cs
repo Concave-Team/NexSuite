50128026f5bc9312a651784516543e1990be65c8
Wizard2D <namamg3@gmail.com>
2023-10-29 01:22:39 +0300

    Initial Commit

---
+++ CloseableTab.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace NexSuite
+{
+    public class CloseableTab : TabItem
+    {
+        CloseableHeader closeableTabHeader;
+        public string Title
+        {
+            set
+            {
+                ((CloseableHeader)this.Header).Label_TabText.Content = value;
+            }
+        }
+
+        protected override void OnSelected(RoutedEventArgs e)
+        {
+            base.OnSelected(e);
+            ((CloseableHeader)this.Header).Button_Exit.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnUnselected(RoutedEventArgs e)
+        {
+            base.OnUnselected(e);
+            ((CloseableHeader)this.Header).Button_Exit.Visibility = Visibility.Hidden;
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            ((CloseableHeader)this.Header).Button_Exit.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            if (!this.IsSelected)
+            {
+                ((CloseableHeader)this.Header).Button_Exit.Visibility = Visibility.Hidden;
+            }
+        }
+
+        // Button MouseEnter - When the mouse is over the button - change color to Red
+        void button_close_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((CloseableHeader)this.Header).Button_Exit.Foreground = Brushes.Red;
+        }
+        // Button MouseLeave - When mouse is no longer over button - change color back to black
+        void button_close_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((CloseableHeader)this.Header).Button_Exit.Foreground = Brushes.Black;
+        }
+        // Button Close Click - Remove the Tab - (or raise
+        // an event indicating a "CloseTab" event has occurred)
+        void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            ((TabControl)this.Parent).Items.Remove(this);
+        }
+        // Label SizeChanged - When the Size of the Label changes
+        // (due to setting the Title) set position of button properly
+        void label_TabTitle_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            
+        }
+
+        public CloseableTab()
+        {
+            // Create an instance of the usercontrol
+            closeableTabHeader = new CloseableHeader();
+            // Assign the usercontrol to the tab header
+            this.Header = closeableTabHeader;
+
+            closeableTabHeader.Button_Exit.MouseEnter +=
+            new MouseEventHandler(button_close_MouseEnter);
+            closeableTabHeader.Button_Exit.MouseLeave +=
+               new MouseEventHandler(button_close_MouseLeave);
+            closeableTabHeader.Button_Exit.Click +=
+               new RoutedEventHandler(button_close_Click);
+            closeableTabHeader.Label_TabText.SizeChanged +=
+               new SizeChangedEventHandler(label_TabTitle_SizeChanged);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NexSuite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SolidColorBrush _BGColor { get { return new SolidColorBrush(Color.FromRgb(51, 51, 51)); } }
+        public StringBuilder ErrorStream = new StringBuilder();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("logs");
+            ErrorStream.AppendLine("Initializing Process...");
+            CloseableTab _SPTabItem = new CloseableTab();
+            _SPTabItem.Name = "StartPageTab";
+            _SPTabItem.Title = "Start Page";
+            TextAreaTabControl.Items.Add(_SPTabItem);
+            _SPTabItem.Focus();
+
+            TextAreaTabControl.SelectionChanged += CTabSelectedCallback;
+        }
+
+        private void CTabSelectedCallback(object sender, SelectionChangedEventArgs e)
+        {
+            if (TextAreaTabControl.SelectedItem is CloseableTab)
+            {
+                var tab = (CloseableTab)TextAreaTabControl.SelectedItem;
+                if (tab.Name == "StartPageTab")
+                {
+                    tab.Content = new StartPageControl();
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            File.WriteAllText("logs/log-" + DateTimeOffset.Now.ToString() + ".txt", ErrorStream.ToString());
+        }
+    }
+}
